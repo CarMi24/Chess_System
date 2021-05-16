69822b652795b4c7ec2744a42c3099ba028ce85c
CarMi24 <83775286+CarMi24@users.noreply.github.com>
2021-05-16 17:03:15 +0300

    Add files via upload

---
+++ Chess_System/headers files/player.h
@@ -0,0 +1,126 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+#include "map.h"
+#include "game.h"
+#include "tournament.h"
+
+#define LOSE 0
+#define DRAW 1
+#define WIN 2
+
+
+/** Type for defining the PlayerResult*/
+typedef enum PlayerResult_t
+{
+    PLAYER_OUT_OF_MEMORY,
+    PLAYER_NULL_ARGUMENT,
+    PLAYER_ERROR,
+    PLAYER_SUCCESS
+}PlayerResult;
+
+/** Type for defining the Player*/
+typedef struct Player_t 
+{
+    int player_id;
+    int total_wins;
+    int total_losses;
+    int total_draws;
+    double level;
+    int total_play_time;
+    Map tournament_rates;
+
+}*Player;
+
+typedef struct TournamentStats_t
+{
+    int wins;
+    int losses;
+    int draws;
+    int total_games_in_tournament;
+} *TournamentStats;
+
+/** Functions for the tournaments map**/
+
+/**
+ * TournamentKey is type of integer pointer - tournament_id.
+ * TournamentData is type of TournamentStats - the stats of the player in the tournament.
+*/
+static MapKeyElement copyTournamentKey(MapKeyElement tournament_key);
+static MapDataElement copyTournamentData(MapDataElement tournament_data);
+static void freeTournamentKey(MapKeyElement tournament_key);
+static void freeTournamentData(MapDataElement tournament_data);
+static int compareKeyTournament(MapKeyElement tournament_key1, MapKeyElement tournament_key2);
+
+
+
+/**
+ * updates the total stats of a player
+ */
+static void updateTotal(int index_outcome);
+
+Player createPlayer(int player_id);
+
+
+void destroyPlayer(Player player);
+
+/**
+ * Calculate and returns the average play time in all the player's games.
+ */
+double calculateAveragePlayTime(Player player);
+
+/**
+ * updates a new outcome of a game to a tournament stats - index_outcome - LOSE 0, DRAW 1, WIN 2
+ * if tournament doesnt exist adds a new one with initial tournament stat.
+ */
+void updatePlayerTournamentStats(Player player, int tournament_id, int index_outcome);
+
+/**
+ * Removes a tournament from the players tournaments maps.
+ * Updates the player's stats (wins, losses, draws).
+ *  @return -
+ * PLAYER_NULL_ARGUMENT
+ */
+PlayerResult removeTournament(Player player, int tournament_id);
+
+/**
+ * returns the players tournaments map.
+ */
+Map getPlayerTournamentsMap(Player player);
+
+/**
+ * Calculate and returns the player general level.
+ */
+double calculatePlayersLevel(Player player, int num_of_games);
+
+/**
+ * returns 0 if never played in the tournament
+ */
+int getGamesPlayedInTournament(Player player, int tournament_id);
+
+/**
+ * Calculate and returns the Player score in a given tournament
+ */
+int getScoreByTournament(Player player, int tournament_id);
+
+/**
+ * Gets 2 players with the same score in the same tournament.
+ * return the id of the one should be considered the winner:
+ * first by comparing wins, then losses, draws and finally ID.
+ */
+int compareSameTournamentScore(Player player1, Player player2);
+
+/**
+ * Calculates and return the players general level in the system
+ */
+double calculatePlayerLevel(Player player, int total_games);
+
+/**
+ * Compare players with the same general level
+ * returns - the player id with the smaller id
+ */
+int compareSameLevelPlayers(Player player1, Player player2);
+
+
+#endif
---
+++ Chess_System/headers files/tournament.h
@@ -0,0 +1,127 @@
+#ifndef TOURNAMENT_H
+#define TOURNAMENT_H
+
+#include "map.h";
+#include "game.h";
+
+#define NULL_ID -1
+
+/** Type for defining the Tournament */
+typedef enum TournamentResult_t
+{
+    TOURNAMENT_OUT_OF_MEMORY,
+    TOURNAMENT_NULL_ARGUMENT,
+    TOURNAMENT_ERROR
+    
+}TournamentResult;
+
+typedef struct Tournament_t
+{
+    int tournament_id;
+    char* tournament_location;
+    Map tournament_games;
+    int max_games_per_player;
+    int tournament_winner_id;
+    int winner_id;
+    int longest_game_time;
+    bool is_closed;
+
+} *Tournament;
+
+
+/** Functions for the games map**/
+
+/**
+ * GameKey is type of integer pointer - the number of the game in the tournament.
+ * GameData is type of Game - games that are taken place in the tournament.
+ * The key will be the tournament_id where the game takes place.
+*/
+static MapKeyElement copyGameKey(MapKeyElement game_key);
+static MapDataElement copyGameData(MapDataElement game);
+static void freeGameKey(MapKeyElement game_key);
+static void freeGameData(MapDataElement game);
+static int compareKeyGame(MapKeyElement game1, MapKeyElement game2);
+
+
+/**
+ * iterates the games map and calculate each player score in the tournament.
+ * at the end the winner's id will be updated in the tournament.
+ */
+static void calculateTournamentWinner(Tournament tournament);
+
+
+/**
+ * updates the longest game time if new_time is bigger.
+ * otherwise does nothing. 
+ */
+static void updateLongestGameTime(int new_time);
+
+
+
+/**
+* createTournament: Allocates a new empty tournament.
+* @return
+* 	NULL - if allocations failed.
+* 	A new Tournament in case of success.
+*/
+Tournament createTournament(int tournament_id, char* location, int max_games);
+
+
+void destroyTournament(Tournament tournament);
+
+/**
+ * Check if the game already taken place in the tournament
+ */
+bool checkIfGameExists(Tournament tournament, Game game);
+
+/**
+ * Adds a game to the games map.
+ */
+TournamentResult addGameToTournament(Tournament tournament, Game game);
+
+/**
+ * Returns the Tournament id
+ * If recieve NULL parameter return -1.
+ */
+int getTournamentID(Tournament tournament);
+
+/**
+ * Iterates the games map and remove the player from each game he plays in.
+ * @return -
+ * TOURNAMENT_NULL_ARGUMENT
+ * TOURNAMENT_ERROR
+ */
+TournamentResult removePlayerFromTournament(Tournament tournament, int player_id);
+
+/**
+ * Closes the tournament - Set the tournament is_closed value to true.
+ * @return -
+ * TOURNAMENT_NULL_ARGUMENT
+ */
+TournamentResult endTournament(Tournament tournament);
+
+/**
+ * returns the tournament longest play time.
+ */
+int getTournamentLongestGameTime(Tournament tournament);
+
+/**
+ * Calculate and returns the tournament average play time.
+ */
+double calculateAverageTournamentGameTime(Tournament tournament);
+
+/**
+ * returns the tournament status: false if open true if closed.
+ */
+bool getTournamentStatus(Tournament Tournament);
+
+/**
+ * prints the tournaments statistics to the a file located in the given path_file.
+ * @return -
+ * TOURNAMENT_NULL_ARGUMENT
+ * TOURNAMENT_ERROR - if tournaments closed
+ */
+TournamentResult printTournamentStats(Tournament tournament,char* path_file);
+
+
+#endif
